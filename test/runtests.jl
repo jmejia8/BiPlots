f81dc0a1090b96d3de4c4d878c527459ef9383af
Jesús Mejía <jesusmejded@gmail.com>
2019-05-26 12:32:57 -0500

    simple tester

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,20 @@ using Bilevel
 using Test
 
 function test1()
-    c = [Bilevel.generateChild(rand(10), rand(10), 1.0i, 2.0i) for i = 1:10]
+    c = [Bilevel.generateChild(rand(3), randn(3).^2, 1.0i, 2.0i) for i = 1:100]
 
     p = scatter(c[1])
-    savefig(p, "tmp.png")
+    # savefig(p, "tmp.png")
 
-    p = scatter(c)
-    savefig(p, "tmp2.png")
+    p =  scatter(c; legend=false)
+    # savefig(p, "tmp2.png")
+
+
+    new_pop(t) = [Bilevel.generateChild(rand(3)/t, randn(3)/t, 1.0i, 2.0i) for i = 1:100]
+    convergence = [Bilevel.State(c[1], new_pop(1.0t)) for t=1:20]
+
+    p = plot(convergence)
+    # gif(p, "tmp.gif")
 
     true
 end
